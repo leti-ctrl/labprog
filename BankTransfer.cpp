f9550db735640445ab4fb425e5683ca7786609e1
leti-ctrl <79322119+leti-ctrl@users.noreply.github.com>
2021-05-31 09:10:41 +0200

    Update BankTransfer.cpp

--- BankTransfer.cpp
+++ BankTransfer.cpp
@@ -1,50 +1,69 @@
 #include "BankTransfer.h"
+#include "Date.h"
 
-BankTransfer::BankTransfer(BankAccount *s, BankAccount *r, float m) : sender(s), recipient(r), money(m) {
+#include <fstream>
+#include <string>
+#include <utility>
+using namespace std;
 
-    //genera il numero della transazione
-    number = rand()%1000+100;
+BankTransfer::BankTransfer(string c, BankAccount *s, BankAccount *r, float m):
+        Transaction(move(c), m), sender(s), recipient(r) {}
 
+void BankTransfer::doTransaction() {
     //apre lo storico dei cc
-    fstream hsend (sender->getOwner(), ios::app);
-    fstream hrec (recipient->getOwner(), ios::app);
-
+    auto fileSender = new fstream (sender->getHistorical(), ios::app);
+    auto fileRecipient = new fstream (recipient->getHistorical(), ios::app);
 
     //sistema i soldi al mittente
-    int legal = sender->isLegalTransaction(money);
+    int legal = sender->isLegalTransaction(amount);
 
     //sistema gli storici
     if (legal == 0) {
-        
+
         //sistema i soldi al destinatario
         if (recipient->checkBankCredit() == 0)
-            recipient->setAvailableBalance(recipient->getAvailableBalance() + money);
+            recipient->setAvailableBalance(recipient->getAvailableBalance() + amount);
         else {
-            recipient->fillBankCredit(money);
+            recipient->fillBankCredit(amount);
         }
 
-        hsend<<"---- Bonifico in uscita n: "<<number<<endl;
-        hsend<<"     A "<<recipient->getName()<<" "<<recipient->getSurname()<<" : -"<<money<<" $"<<endl;
+        *fileSender << "---- BONIFICO - Operazione n: " << numberTransaction << endl;
 
-        hrec<<"++++ Bonifico in entrata n: "<<number<<endl;
-        hrec<<"     Da "<<sender->getName()<<" "<< sender->getSurname()<<" : +"<<money<<" $"<<endl;
+        *fileRecipient << "++++ BONIFICO - Operazione n: " << numberTransaction <<
+                       " di " << sender->getOwner() << " conto numero: " << sender->getNumberAccount() << endl;
+
     }
 
     else {
-        hsend << "ERR- Bonifico in uscita : -" << money << " $  -FALLITO- " << endl;
-        hsend << "     A " << recipient->getName() << " " << recipient->getSurname() << " : -" << money << " $" << endl;
+        *fileSender << "-ERROR- BONIFICO IN USCITA - Operazione n: " << numberTransaction << endl;
 
-        hrec<<"ERR- Bonifico in entrata n: "<<number<<endl;
-        hrec<<"     Da "<<sender->getName()<<" "<< sender->getSurname()<<" : +"<<money<<" $"<<endl;
+        *fileRecipient << "-ERROR- BONIFICO IN ENTRATA - Operazione n: " << numberTransaction << endl;
+
     }
 
-    hsend.close();
-    hrec.close();
+    auto date = new Date;
+
+    *fileSender << "     Intestato a " << recipient->getOwner() <<
+                " sul conto " << recipient->getNumberAccount() << endl;
+    *fileSender << "     Causale: " << causal << endl;
+    *fileSender << "     Importo: " << amount << " $" << endl;
+    *fileSender << "     Data contabile: " << date->getCurrentDate() << endl;
+    *fileSender << endl;
+    fileSender->close();
+    delete fileSender;
+
+    *fileRecipient << "     Causale: " << causal << endl;
+    *fileRecipient << "     Importo: " << amount << " $" << endl;
+    *fileRecipient << "     Data contabile: " << date->getCurrentDate() << endl;
+    *fileRecipient << endl;
+    fileRecipient->close();
+    delete fileRecipient;
+
+    delete date;
 
     sender->printAvailableBalance();
     sender->printRealBankCredit();
 
     recipient->printAvailableBalance();
     recipient->printRealBankCredit();
-
 }
